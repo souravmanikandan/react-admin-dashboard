bf46070e332ebfc07e4c9d11b4ed05ad6c966ad9
smcoderr <souravoz2018@gmail.com>
2025-03-13 12:03:59 +0530

    OverviewCards

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Header from '../components/common/Header'
+import OverviewCards from '../components/analytics/OverviewCards'
 
 const AnalyticsPage = () => {
   return (
-    <div>AnalyticsPage</div>
+    <div className='flex-1 overflow-auto relative z-10 bg-gray-900'>
+        <Header title={"Analytics Dashboard"} />
+
+        <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+            <OverviewCards />
+        </main>
+    </div>
   )
 }
 
