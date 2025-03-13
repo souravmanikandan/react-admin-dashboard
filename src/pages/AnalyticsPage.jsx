45266ab1f6751beb25df5ad0d82a28166925ba7f
smcoderr <souravoz2018@gmail.com>
2025-03-13 12:23:41 +0530

    ProductPerformance

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Header from '../components/common/Header'
 import OverviewCards from '../components/analytics/OverviewCards'
 import RevenueChart from '../components/analytics/RevenueChart'
+import ChannelPerformance from '../components/analytics/ChannelPerformance'
+import ProductPerformance from '../components/analytics/ProductPerformance'
+import UserRetention from '../components/analytics/UserRetention'
+import CustomerSegmentation from '../components/analytics/CustomerSegmentation'
 
 const AnalyticsPage = () => {
   return (
