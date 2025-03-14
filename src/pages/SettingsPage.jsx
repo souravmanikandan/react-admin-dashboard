724d0ce7dd37032178588c2f8f0b11d87ece8fae
smcoderr <souravoz2018@gmail.com>
2025-03-14 18:33:26 +0530

    ToggleSwitch

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/common/Header'
 import Profile from '../components/settings/Profile'
+import Notifications from '../components/settings/Notifications'
 
 const SettingsPage = () => {
   return (
@@ -8,6 +9,7 @@ const SettingsPage = () => {
         <Header title="Settings" />
         <main className='max-w-4xl mx-auto py-6 px-4 lg:px-8'>
             <Profile />
+            <Notifications />
         </main>
     </div>
   )
